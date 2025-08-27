a777652377d4025cb4136b4127453b7dec6c24ee
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-08-26 23:18:55 -0300

    feat(controller): Import service in controller

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -1,11 +1,22 @@
 import { IncomingMessage, ServerResponse } from "http";
+import {serviceListEpisodio} from "../services/lsit-episodio-service"
+import {serviceFilterEpisodio} from "../services/filter-episodios-service"
 
 
-export const getListEpisodes = (request: IncomingMessage, response: ServerResponse)=>{
+export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse)=>{
+    const data = await serviceListEpisodio();
     response.writeHead(200,{"content-type":"application/json"});
     response.end(
-        JSON.stringify({
-            name:"Gabriel"
-        })
-    )
+        JSON.stringify(data)
+    );
+}
+
+export const getFilterconsult = async (request: IncomingMessage, response: ServerResponse) => {
+    const queryString = request.url?.split("?p=")[1] || "";
+    const data = await serviceFilterEpisodio(queryString);
+    response.writeHead(200,{"content-type":"application/json"});
+    response.end(
+        JSON.stringify(data)
+    );
+
 }
