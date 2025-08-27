bb61b00fb8609ba529b6f916ca7688550a043ca4
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-08-26 23:20:39 -0300

    feat(model): Add model of data

---
+++ src/model/podcasts-model.ts
@@ -0,0 +1,9 @@
+
+export interface podcastModel {
+    podcastsName:string,
+    episodio:string,
+    videoId:string,
+    // imgCapa:"https://i.ytimg.com/vi/Gd6aSyT2NqE/maxresdefault.jpg",
+    // link:"https://www.youtube.com/live/Gd6aSyT2NqE?si=tOwDGdFXnwDypKx1",
+    categories:string[]
+}
